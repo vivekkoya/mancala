baf392f0c7a8999066811e24e1a972c9ab0b46ce
ETPH28 <87104268+ETPH28@users.noreply.github.com>
2022-11-27 00:57:35 -0800

    End condition, Undo logic

--- Mancala_4Sure/src/BoardData.java
+++ Mancala_4Sure/src/BoardData.java
@@ -1,12 +1,15 @@
 import java.util.*;
 
 import javax.swing.event.InternalFrameEvent;
-
+/**
+ * author
+ */
 public class BoardData {
     final int INITIAL_MARBLES = 4;
     final int BOARD_LENGTH = 7;
     private ArrayList<ArrayList<Integer>> board;
     private boolean isP1Turn;
+    private boolean gameOver;
     private LinkedList<ArrayList<ArrayList<Integer>>> boardStack;
     private LinkedList<Boolean> turnStack;
     /**
@@ -25,6 +28,7 @@ public class BoardData {
         //board.get(1) is player 2's side
         board.add(p2);
         isP1Turn=true;
+        gameOver = false;
         boardStack = new LinkedList<ArrayList<ArrayList<Integer>>>();
         turnStack = new LinkedList<Boolean>();
     }
@@ -34,8 +38,13 @@ public class BoardData {
     public int getPit(int side, int pit){
         return board.get(side%2).get(pit);
     }
-    //conducts an action
+    /**
+     * conducts an action in the game
+     * @param pit- pit to make action with
+     */
     public void turn(int pit){
+        boardStack.add(board);
+        turnStack.add(isP1Turn);
         //keeps track of the side
         int side;
         if(isP1Turn){
@@ -84,9 +93,36 @@ public class BoardData {
             incPit(sideC, 6, steal+1);
         }
     }
+    public void undo(){
+        isP1Turn = turnStack.pop();
+        board = boardStack.pop();
+    }
     private void incPit(int side, int pit, int inc){
         int temp = board.get(side%2).get(pit);
         temp+=inc;
         board.get(side%2).set(pit, temp);
+    }
+    private void endGame(){
+        int r1=0;
+        int r2=0;
+        for(int i=0; i<BOARD_LENGTH-1; i++){
+            r1 += board.get(0).get(i);
+            r2 += board.get(1).get(i);
+        }
+        if(r1==0){
+            boardWipe(1,r2);
+        }
+        else if(r2==0){
+            boardWipe(0,r1);
+        }
+    }
+    private void boardWipe(int side, int amt){
+        gameOver=true;
+        int points=board.get(side).get(BOARD_LENGTH-1);
+        points+=amt;
+        board.get(side).set(BOARD_LENGTH-1, points);
+        for(int i=0; i<BOARD_LENGTH-1; i++){
+            board.get(side).set(i, 0);
+        }
     }
 }
