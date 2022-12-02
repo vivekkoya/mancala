9550c008d4d80a6e01fbf8207082ce8f420556b7
vivekkoya <67130044+vivekkoya@users.noreply.github.com>
2022-12-01 22:13:25 -0800

    Draw buttons, set outer pits to unclickable

--- Mancala_4Sure/src/DisplayBoard.java
+++ Mancala_4Sure/src/DisplayBoard.java
@@ -11,128 +11,147 @@ import javax.swing.*;
 
 public class DisplayBoard {
 
-    private JFrame frame;
+	private JFrame frame;
 
-    private void show() {
-        this.frame.pack();
-        this.frame.setVisible(true);
-    }
+	private void show() {
+		this.frame.pack();
+		this.frame.setVisible(true);
+	}
 
-    /**
-     * Launch the application.
-     */
-    public static void main(String[] args) {
-        EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                try {
-                    DisplayBoard window = new DisplayBoard();
-                    window.show();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-    }
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					DisplayBoard window = new DisplayBoard();
+					window.show();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 
-    /**
-     * Create the application.
-     */
-    public DisplayBoard() {
+	/**
+	 * Create the application.
+	 */
+	public DisplayBoard() {
 
-        try {
-            initialize();
-        } catch (IOException e) {
-            System.err.println("Failed to initialize becaus eof IOException");
-            e.printStackTrace();
-        }
-    }
+		try {
+			initialize();
+		} catch (IOException e) {
+			System.err.println("Failed to initialize becaus eof IOException");
+			e.printStackTrace();
+		}
+	}
 
-    /**
-     * Initialize the contents of the frame.
-     */
-    private void initialize() throws IOException {
-        frame = new JFrame();
-        // frame.setBounds(100, 100, 450, 300);
-        // frame.se`
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        GridBagLayout gridBagLayout = new GridBagLayout();
-        // gridBagLayout.columnWidths = new int[] { 0, 8 };
-        // gridBagLayout.rowHeights = new int[] { 0, 1 };
-        // gridBagLayout.columnWeights = new double[] { 1.0, Double.MIN_VALUE };
-        // gridBagLayout.rowWeights = new double[] { 1.0, Double.MIN_VALUE };
-        frame.getContentPane().setLayout(gridBagLayout);
-        GridBagConstraints gbc_leftouter = new GridBagConstraints();
-        gbc_leftouter.fill = GridBagConstraints.VERTICAL;
-        gbc_leftouter.gridx = 0;
-        gbc_leftouter.gridy = 0;
-        gbc_leftouter.gridheight = 2;
-        gbc_leftouter.weighty = 2;
-        GridBagConstraints gbc_rightouter = new GridBagConstraints();
-        gbc_rightouter.fill = GridBagConstraints.VERTICAL;
-        gbc_rightouter.gridx = 7;
-        gbc_rightouter.gridy = 0;
-        gbc_rightouter.gridheight = 2;
-        gbc_rightouter.weighty = 2;
-        GridBagConstraints gbc_innertop = new GridBagConstraints(), gbc_innerbottom = new GridBagConstraints();
-        Pit[] gpc_top = new Pit[6], gpc_bottom = new Pit[6];
-        Pit lh = new Pit(), rh =lh;
-        for (int i = 0; i < 8; i++) {
-            JPanel panel = new JPanel();
-            panel.setBackground(UIManager.getColor("CheckBoxMenuItem.acceleratorForeground"));
-            if (i == 0) {
-                // Set constraints to gbc_leftouter
-                // panel.add(new JButton("Left holder"));
-                lh = new Pit("Left holder");
-                lh.setPreferredSize(new Dimension(250, 400));
-                frame.getContentPane().add(lh, gbc_leftouter);
-            } else if (i == 7) {
-                // Set constraints to gbc_leftouter
-                // panel.add(new JButton("Right holder"));
-                rh = new Pit("Right holder");
-                rh.setPreferredSize(new Dimension(250, 400));
-                frame.getContentPane().add(rh, gbc_rightouter);
-            } else {
-                // Set constraints to gbc_inner
-                // panel.add(new JButton("Inner Pit"));
-                // GridBagConstraints gbc_innertop = new GridBagConstraints();
-                gbc_innertop.gridx = i;
-                gbc_innertop.gridy = 0;
-                gbc_innertop.insets = new Insets(0, 5, 0, 5);
-                gbc_innertop.gridheight = 1;
-                gbc_innertop.weighty = 1;
-                // top pits
-                gpc_top[i - 1] = new Pit();
-                // Pit jButtonTop = new Pit();
-                gpc_top[i - 1].setMarbles(4);
-                frame.getContentPane().add(gpc_top[i - 1], gbc_innertop);
-                gpc_bottom[i - 1] = new Pit();
-                gpc_bottom[i - 1].setMarbles(4);
-                // GridBagConstraints gbc_innerbottom = new GridBagConstraints();
-                gbc_innerbottom.gridx = i;
-                gbc_innerbottom.gridy = 1;
-                gbc_innerbottom.insets = new Insets(0, 5, 0, 5);
-                gbc_innerbottom.gridheight = 1;
-                gbc_innerbottom.weighty = 1;
-                // bottom pits
-                // JButton jButtonBottom = new Pit();
-                frame.getContentPane().add(gpc_bottom[i - 1], gbc_innerbottom);
-            }
-        }
-        gbc_innerbottom.fill = GridBagConstraints.HORIZONTAL;
-        gbc_innerbottom.gridx = 0;
-        gbc_innerbottom.gridy = 2;
-        gbc_innertop.insets = new Insets(0, 2, 0, 2);
-        gbc_innerbottom.gridheight = 1;
-        gbc_innerbottom.weighty = 1;
-        JButton reset = new JButton("undo");
-        frame.getContentPane().add(reset, gbc_innerbottom);
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() throws IOException {
+		frame = new JFrame();
+		// frame.setBounds(100, 100, 450, 300);
+		// frame.se`
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		// gridBagLayout.columnWidths = new int[] { 0, 8 };
+		// gridBagLayout.rowHeights = new int[] { 0, 1 };
+		// gridBagLayout.columnWeights = new double[] { 1.0, Double.MIN_VALUE };
+		// gridBagLayout.rowWeights = new double[] { 1.0, Double.MIN_VALUE };
+		frame.getContentPane().setLayout(gridBagLayout);
+		GridBagConstraints gbc_leftouter = new GridBagConstraints();
+		gbc_leftouter.fill = GridBagConstraints.VERTICAL;
+		gbc_leftouter.gridx = 0;
+		gbc_leftouter.gridy = 0;
+		gbc_leftouter.gridheight = 2;
+		gbc_leftouter.weighty = 2;
+		GridBagConstraints gbc_rightouter = new GridBagConstraints();
+		gbc_rightouter.fill = GridBagConstraints.VERTICAL;
+		gbc_rightouter.gridx = 7;
+		gbc_rightouter.gridy = 0;
+		gbc_rightouter.gridheight = 2;
+		gbc_rightouter.weighty = 2;
+		GridBagConstraints gbc_innertop = new GridBagConstraints(), gbc_innerbottom = new GridBagConstraints();
+		Pit[] gpc_top = new Pit[6], gpc_bottom = new Pit[6];
+		Pit lh = new Pit(), rh = lh;
 
-        // GridBagConstraints gbc_panel = new GridBagConstraints();
-        // gbc_panel.fill = GridBagConstraints.HORIZONTAL;
-        // gbc_panel.gridx = 0;
-        // gbc_panel.gridy = 0;
-        // frame.getContentPane().add(panel, gbc_panel);
-    }
-    // lh.setMarbles(2);
+		for (int i = 0; i < 8; i++) {
+			JPanel panel = new JPanel();
+			panel.setBackground(UIManager.getColor("CheckBoxMenuItem.acceleratorForeground"));
+			if (i == 0) {
+				// Set constraints to gbc_leftouter
+				// panel.add(new JButton("Left holder"));
+				lh = new Pit("Left holder");
+				lh.setPreferredSize(new Dimension(250, 400));
+				frame.getContentPane().add(lh, gbc_leftouter);
+			} else if (i == 7) {
+				// Set constraints to gbc_leftouter
+				// panel.add(new JButton("Right holder"));
+				rh = new Pit("Right holder");
+				rh.setPreferredSize(new Dimension(250, 400));
+				frame.getContentPane().add(rh, gbc_rightouter);
+			} else {
+				// Set constraints to gbc_inner
+				// panel.add(new JButton("Inner Pit"));
+				// GridBagConstraints gbc_innertop = new GridBagConstraints();
+				gbc_innertop.gridx = i;
+				gbc_innertop.gridy = 0;
+				gbc_innertop.insets = new Insets(0, 5, 0, 5);
+				gbc_innertop.gridheight = 1;
+				gbc_innertop.weighty = 1;
+				// top pits
+				gpc_top[i - 1] = new Pit();
+				// Pit jButtonTop = new Pit();
+				gpc_top[i - 1].setMarbles(4);
+				frame.getContentPane().add(gpc_top[i - 1], gbc_innertop);
+				gpc_bottom[i - 1] = new Pit();
+				gpc_bottom[i - 1].setMarbles(4);
+				// GridBagConstraints gbc_innerbottom = new GridBagConstraints();
+				gbc_innerbottom.gridx = i;
+				gbc_innerbottom.gridy = 1;
+				gbc_innerbottom.insets = new Insets(0, 5, 0, 5);
+				gbc_innerbottom.gridheight = 1;
+				gbc_innerbottom.weighty = 1;
+				// bottom pits
+				// JButton jButtonBottom = new Pit();
+				frame.getContentPane().add(gpc_bottom[i - 1], gbc_innerbottom);
+			}
+		}
+		/**
+		 * @see SetColor in progress
+		 */
+		lh.setEnabled(false);
+		lh.setMarbles(2);
+//		lh.setBorderPainted(true);
+		lh.setColor(new Color(255, 0, 0));
+
+		rh.setEnabled(false);
+		rh.setMarbles(2);
+
+		gbc_innerbottom.fill = GridBagConstraints.HORIZONTAL;
+		gbc_innerbottom.gridx = 0;
+		gbc_innerbottom.gridy = 2;
+		gbc_innertop.insets = new Insets(0, 2, 0, 2);
+		gbc_innerbottom.gridheight = 1;
+		gbc_innerbottom.weighty = 1;
+		JButton reset = new JButton("undo");
+		reset.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// trigger undo logic in BoardData
+
+			}
+		});
+		// reset.addActionListener(new actionListener());
+		frame.getContentPane().add(reset, gbc_innerbottom);
+
+		// GridBagConstraints gbc_panel = new GridBagConstraints();
+		// gbc_panel.fill = GridBagConstraints.HORIZONTAL;
+		// gbc_panel.gridx = 0;
+		// gbc_panel.gridy = 0;
+		// frame.getContentPane().add(panel, gbc_panel);
+	}
+	// lh.setMarbles(2);
 
 }
--- Mancala_4Sure/src/Pit.java
+++ Mancala_4Sure/src/Pit.java
@@ -19,117 +19,92 @@ import javax.swing.JButton;
 import javax.swing.border.Border;
 
 public class Pit extends JButton {
+	Color pitcolor = Color.black;
 
-    private static final long serialVersionUID = 1L;
-    ArrayList<Ellipse2D.Double> marbles = new ArrayList<>();
-    int numOfMarbles = 0;
-    private int oldMarbleCount = 0;
+	private static final long serialVersionUID = 1L;
+	ArrayList<Ellipse2D.Double> marbles = new ArrayList<>();
+	int numOfMarbles = 0;
+	private int oldMarbleCount = 0;
 
-    /**
-     * Create the panel.
-     *
-     * @throws IOException
-     */
-    public Pit() throws IOException {
+	/**
+	 * Create the panel.
+	 *
+	 * @throws IOException
+	 */
+	public Pit() throws IOException {
 
-        // setBackground(new Color(Color.TRANSLUCENT));
-        setPreferredSize(new Dimension(200, 200));
-        setBorder(new RoundedBorder(37));
-        setOpaque(true);
-    }
+		// setBackground(new Color(Color.TRANSLUCENT));
+		setPreferredSize(new Dimension(200, 200));
+		setBorder(new RoundedBorder(37));
+		setOpaque(true);
+	}
 
-    public Pit(String string) {
-        super(string);
-    }
+	public Pit(String string) {
+		super(string);
+	}
 
-    void setMarbles(int num) {
-        numOfMarbles = num;
-        // Point p = get
-        // Ellipse2D.Double marble = new Ellipse2D.Double(p.getX(), p.getY(), 34.22,
-        // 34.22);
-        // marbles.add(marble);
-        // marbles.clear();
-        // Random location = new Random();
-        // for (int i = 0; i < numOfMarbles; ++i) {
-        // double x = location.nextDouble(0, getWidth());
-        //// Random color = new Random();
-        // double y = location.nextDouble(0, getHeight());
-        //// g2.setColor(new Color(color.nextInt(0, 240), color.nextInt(0, 240),
-        // color.nextInt(0, 240)));
-        // Ellipse2D.Double marble = new Ellipse2D.Double(x, y, 20, 20);
-        // marbles.add(marble);
-        // }
-    }
+	void setMarbles(int num) {
+		numOfMarbles = num;
+	}
 
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        Graphics2D g2 = (Graphics2D) g;
-        URL file;
-        try {
-            // file = new URL(
-            // "file:///home/surikoya/Documents/Fall%202022/classfiles/Eclipse-Workspace/Mancala/src/marble-big.jpg");
-            // Image img = ImageIO.read(file);
-            // int w = img.getWidth(null);
-            // int h = img.getHeight(null);
-            // BufferedImage bi = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
-            // g.drawImage(img, 0, 0, null);
-            // g.drawImage(img, 40, 0, null);
-            g2.setColor(Color.blue);
-            Point p = getLocation();
-            Random location = new Random();
+	void setColor(Color c) {
+		pitcolor = c;
+	}
 
-            // g2.setColor(new Color(color.nextInt(0, 240), color.nextInt(0, 240),
-            // color.nextInt(0, 240)));
-            // marbles.add(marble);
-            // marbles.clear();
-            if (oldMarbleCount != numOfMarbles) {
-                for (int i = oldMarbleCount; i < numOfMarbles; ++i) {
-                    double x = location.nextInt(getWidth());
-                    Random color = new Random();
-                    double y = location.nextInt(getHeight());
-                    Ellipse2D.Double marble = new Ellipse2D.Double(x, y, 20, 20);
-                    marbles.add(marble);
-                }
-            }
-            // repaint();
-            for (Ellipse2D.Double c : marbles) {
-                g2.fill(c);
-            }
-            g2.setColor(Color.black);
-            oldMarbleCount = numOfMarbles;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-    // public void paintIcon(Component c, Graphics g, int x, int y) throws
-    // IOException {
-    // Graphics2D g2d = (Graphics2D) g.create();
-    // URL file = new URL("file:///home/surikoya/Downloads/shadedcircle.jpg");
-    // Image img = ImageIO.read(file);
-    // g2d.drawImage(img, x, y, c);
-    // repaint();
-    //// g2d.dispose();
-    // }
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		Graphics2D g2 = (Graphics2D) g;
 
-    private static class RoundedBorder implements Border {
+		try {
 
-        private int radius;
+			g2.setColor(Color.blue);
+			Point p = getLocation();
+			Random location = new Random();
 
-        RoundedBorder(int radius) {
-            this.radius = radius;
-        }
+			// g2.setColor(new Color(color.nextInt(0, 240), color.nextInt(0, 240),
+			// color.nextInt(0, 240)));
+			// marbles.add(marble);
+			// marbles.clear();
+			if (oldMarbleCount != numOfMarbles) {
+				for (int i = oldMarbleCount; i < numOfMarbles; ++i) {
+					double x = location.nextInt((getWidth() * 6) / 7);
+					// Random color = new Random();
+					double y = location.nextInt((getHeight() * 6) / 7);
+					Ellipse2D.Double marble = new Ellipse2D.Double(x, y, 20, 20);
+					marbles.add(marble);
+				}
+			}
+			// repaint();
+			for (Ellipse2D.Double c : marbles) {
+				g2.fill(c);
+			}
+			g2.setColor(pitcolor);
+			oldMarbleCount = numOfMarbles;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-        public Insets getBorderInsets(Component c) {
-            return new Insets(this.radius + 1, this.radius + 1, this.radius + 2, this.radius);
-        }
 
-        public boolean isBorderOpaque() {
-            return true;
-        }
+	private static class RoundedBorder implements Border {
 
-        public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
-            g.drawRoundRect(x, y, width - 1, height - 1, radius, radius);
-        }
-    }
+		private int radius;
+
+		RoundedBorder(int radius) {
+			this.radius = radius;
+		}
+
+		public Insets getBorderInsets(Component c) {
+			return new Insets(this.radius + 1, this.radius + 1, this.radius + 2, this.radius);
+		}
+
+		public boolean isBorderOpaque() {
+			return true;
+		}
+
+		public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+			g.drawRoundRect(x, y, width - 1, height - 1, radius, radius);
+		}
+	}
 
 }
