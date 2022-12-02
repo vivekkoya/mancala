b134229fd29fe45cb317f887fe74cb38886a9368
c00lme <ksrivatsa19@gmail.com>
2022-12-01 21:06:06 -0800

    put package src

--- Mancala_4Sure/src/BoardData.java
+++ Mancala_4Sure/src/BoardData.java
@@ -1,4 +1,4 @@
-
+package src; 
 import java.util.*;
 
 import javax.swing.event.InternalFrameEvent;
--- Mancala_4Sure/src/Pit.java
+++ Mancala_4Sure/src/Pit.java
@@ -1,4 +1,4 @@
-
+package src; 
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
