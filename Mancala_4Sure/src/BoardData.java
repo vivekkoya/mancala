d67f5505dcc01cd8d0c6d86f38c6a004e85f8d47
vivekkoya <67130044+vivekkoya@users.noreply.github.com>
2022-12-02 16:05:34 -0800

    Add undo logic to board with src package header

--- Mancala_4Sure/src/BoardData.java
+++ Mancala_4Sure/src/BoardData.java
@@ -1,95 +1,152 @@
-package src; 
+package src;
 import java.util.*;
 
 import javax.swing.event.InternalFrameEvent;
-
+/**
+ * author
+ */
 public class BoardData {
-
-    final int INITIAL_MARBLES = 4;
     final int BOARD_LENGTH = 7;
+    private int initialMarbles;
     private ArrayList<ArrayList<Integer>> board;
     private boolean isP1Turn;
+    private boolean gameOver;
     private LinkedList<ArrayList<ArrayList<Integer>>> boardStack;
-    private LinkedList<Boolean> turnStack;
-
+    private boolean isP1PrevTurn;
+    private int numUndos;
+    private boolean consecUndo;
     /**
      * Constructor generates a default board
      */
-    public BoardData() {
+    public BoardData(int initMarbles){
+        initialMarbles = initMarbles;
         ArrayList<Integer> p1 = new ArrayList<Integer>();
-        for (int index = 0; index < BOARD_LENGTH - 1; index++) {
-            p1.add(INITIAL_MARBLES);
+        for (int index = 0; index < BOARD_LENGTH-1; index++) {
+            p1.add(initialMarbles);
         }
         p1.add(0);
         ArrayList<Integer> p2 = new ArrayList<Integer>(p1);
         board = new ArrayList<ArrayList<Integer>>();
-        // board.get(0) is player 1's side
+        //board.get(0) is player 1's side
         board.add(p1);
-        // board.get(1) is player 2's side
+        //board.get(1) is player 2's side
         board.add(p2);
-        isP1Turn = true;
+        isP1Turn=true;
+        gameOver = false;
         boardStack = new LinkedList<ArrayList<ArrayList<Integer>>>();
-        turnStack = new LinkedList<Boolean>();
+        isP1PrevTurn = true;
+        numUndos = 0;
+        consecUndo=false;
     }
-
-    public ArrayList<ArrayList<Integer>> getBoard() {
+    public ArrayList<ArrayList<Integer>> getBoard(){
         return board;
     }
-
-    public int getPit(int side, int pit) {
-        return board.get(side % 2).get(pit);
+    public int getPit(int side, int pit){
+        return board.get(side%2).get(pit);
     }
-
-    // conducts an action
-    public void turn(int pit) {
-        // keeps track of the side
-        int side;
-        if (isP1Turn) {
-            side = 0;
-        } else {
-            side = 1;
+    /**
+     * conducts an action in the game
+     * @param pit- pit to make action with
+     */
+    public void turn(int pit){
+        if(isP1Turn != isP1PrevTurn){
+            boardStack.clear();
         }
-        // number of stones grabbed
-        int i = board.get(side % 2).get(pit);
-        // index of the pits
+        consecUndo = false;
+        boardStack.add(board);
+        isP1PrevTurn = isP1Turn;
+        //keeps track of the side
+        int initSide;
+        if(isP1Turn){
+            initSide=0;
+        }
+        else{
+            initSide=1;
+        }
+        int side = initSide;
+        //number of stones grabbed
+        int i = board.get(side%2).get(pit);
+        //index of the pits
         int j = pit;
-        board.get(side % 2).set(pit, 0);
-        while (i > 0) {
+        board.get(side%2).set(pit, 0);
+        while(i>0){
             j++;
-            if (j == BOARD_LENGTH - 1) {
-                if (isP1Turn == (side % 2 == 0)) {
-                    incPit(side, j, 1);
+            if(j==BOARD_LENGTH-1){
+                if(isP1Turn == (side%2==0)){
+                    incPit(side, j,1);
                     i--;
                 }
-                j = -1;
+                j=-1;
                 side++;
                 i++;
-            } else {
-                incPit(side, j, 1);
+            }
+            else{
+                incPit(side, j,1);
             }
             i--;
         }
         j++;
         isP1Turn = !isP1Turn;
-        if (j == 0 && ((side % 2 == 0) == isP1Turn)) {
+        if(j==0 && ((side%2==0)==isP1Turn)){
             isP1Turn = !isP1Turn;
-        } else if (getPit(side, j - 1) == 1) {
-            int steal = getPit(side + 1, 6 - j + 1);
-            board.get((side + 1) % 2).set(6 - j + 1, 0);
-            board.get(side % 2).set(j - 1, 0);
+        }
+        else if((initSide == side % 2) && getPit(side, j-1)==1){
+            int steal = getPit(side+1, 6-j+1);
+            board.get((side+1)%2).set(6-j+1, 0);
+            board.get(side%2).set(j-1, 0);
             int sideC;
-            if (isP1Turn) {
-                sideC = 1;
-            } else {
-                sideC = 0;
+            if(isP1Turn){
+                sideC=1;
             }
-            incPit(sideC, 6, steal + 1);
+            else{
+                sideC=0;
+            }
+            incPit(sideC, 6, steal+1);
+        }
+        endGame();
+    }
+    /**
+     * undos a turn
+     */
+    public void undo(){
+        board = boardStack.pop();
+        isP1Turn = isP1PrevTurn;
+    }
+    public boolean canUndo(){
+        if(!(numUndos>=3) && boardStack.size()>0 && !consecUndo){
+            numUndos++;
+            consecUndo = true;
+            return true;
+        }
+        return false;
+    }
+    private void incPit(int side, int pit, int inc){
+        int temp = board.get(side%2).get(pit);
+        temp+=inc;
+        board.get(side%2).set(pit, temp);
+    }
+    private void endGame(){
+        int r1=0;
+        int r2=0;
+        for(int i=0; i<BOARD_LENGTH-1; i++){
+            r1 += board.get(0).get(i);
+            r2 += board.get(1).get(i);
+        }
+        if(r1==0){
+            boardSweep(1,r2);
+        }
+        else if(r2==0){
+            boardSweep(0,r1);
         }
     }
-
-    private void incPit(int side, int pit, int inc) {
-        int temp = board.get(side % 2).get(pit);
-        temp += inc;
-        board.get(side % 2).set(pit, temp);
+    private void boardSweep(int side, int amt){
+        gameOver=true;
+        int points=board.get(side).get(BOARD_LENGTH-1);
+        points+=amt;
+        board.get(side).set(BOARD_LENGTH-1, points);
+        for(int i=0; i<BOARD_LENGTH-1; i++){
+            board.get(side).set(i, 0);
+        }
     }
 }
+
